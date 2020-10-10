8740ee7b6004ce137a4bfa00df02f117fcb9f48e
seoe5580 <seoe5580@wrdsb.ca>
2020-10-10 05:19:51 +0000

    Done average height question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Gets the average height based on user's data and tells the user which height is above average
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,41 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user how much heights they want to enter
+    System.out.println("How many heights would you like to enter");
+
+    // Gets the the amount of heights they want to enter
+    int heightNumber = input.nextInt();
+    
+    int[] height = new int[heightNumber];
+
+    System.out.println("Please enter the heights on separate lines");
+
+    for(int i = 0; i < height.length; i++){
+      height[i] = input.nextInt();
+    }
+    
+    int total = 0;
+
+    for(int i = 0; i < height.length; i++){
+      total = total + height[i];
+    }
+
+    int average = total / height.length;
+
+    System.out.println("The average height is " + average + "cm");
+
+    System.out.println("The heights above average are");
+
+    for(int i = 0; i < height.length; i++){
+      if(height[i] > average){
+        System.out.println(height[i]);
+      }
+    }
+    
     
   }
 }
